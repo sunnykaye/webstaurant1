7907b0b8b1506b7df2f7c485ec76e1a69c41d0c1
sunnykaye <126847091+sunnykaye@users.noreply.github.com>
2023-05-29 19:09:13 -0400

    529709 final

--- test1.java
+++ test1.java
@@ -11,12 +11,13 @@ public class WebstaurantStoreAutomation {
         WebDriver d = new ChromeDriver();
 
         try {
-            driver.get("https://www.webstaurantstore.com/");
+            d.get("https://www.webstaurantstore.com/");
 
             WebElement search = d.findElement(By.id("searchval"));
             search.sendKeys("stainless work table");
             search.submit();
 
+            // Validate all  titles have "table"
             boolean allTable = true;
             java.util.List<WebElement> titles = d.findElements(By.xpath("//a[@class='description']"));
             for (WebElement t : titles) {
@@ -26,3 +27,35 @@ public class WebstaurantStoreAutomation {
                 }
             }
             System.out.println("Table is in all titles: " + allTable);
+
+
+            // Add last item to cart
+            WebElement product = titles.get(titles.size() - 1);
+            product.click();
+            WebElement addButton = d.findElement(By.id("buyButton"));
+            addButton.click();
+
+            WebDriverWait wait = new WebDriverWait(d, 10);
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("cartItemCountSpan"), "1"));
+
+
+
+            // Empty Cart
+            WebElement cartButton = d.findElement(By.id("cartItemCountSpan"));
+            cartButton.click();
+            WebElement emptyButton = d.findElement(By.xpath("//a[@class='emptyCartButton']"));
+            emptyButton.click();
+
+            System.out.println("Cart is clear");
+        } finally {      
+             d.quit();
+        } 
+    }
+
+    /* Hi there! I usually code with javascript with Mocha, 
+    but here is my submission for Java and Maven. 
+    Thank you for your time and consideration.
+    Sunny Khandokar.
+    */ 
+
+}
